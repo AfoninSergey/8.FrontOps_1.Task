5632118532cba9e11c309e4f929caed6b90dc5c7
Afonin Sergey <afoninsergey@mail.ru>
2025-10-09 12:24:54 +0300

    feat: Adds frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+	return (
+		<div className="App">
+			<header className="App-header">
+				<h1 className="animated-title">Модуль 8. FrontOps / Задание #3</h1>
+				<img src={logo} className="App-logo" alt="logo" />
+			</header>
+		</div>
+	);
+}
+
+export default App;
